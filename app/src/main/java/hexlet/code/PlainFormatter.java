a4616466a6e041901d476c157548171183cde2ba
Denis Makarov <d.makarov44@yandex.ru>
2024-09-17 12:46:04 +0300

    edit

--- app/src/main/java/hexlet/code/PlainFormatter.java
+++ app/src/main/java/hexlet/code/PlainFormatter.java
@@ -8,42 +8,57 @@ public class PlainFormatter {
     public static String format(List<Map<String, Object>> compareList) {
         StringJoiner stringJoiner = new StringJoiner("\n");
         for (Map<String, Object> map : compareList) {
-            StringBuilder next = new StringBuilder("Property " + "'" + map.get("NAME").toString() + "' was ");
+            StringBuilder next = new StringBuilder();
+            String begin = "Property  '" + map.get("NAME").toString() + "' was ";
             String status = map.get("STATUS").toString();
 
             switch (status) {
                 case "DELETED":
+                    next.append(begin);
                     next.append("removed");
+                    stringJoiner.add(next);
                     break;
                 case "ADD":
-                    next.append("added with value: ").append(map.get("NEW VALUE") != null ? map.get("NEW VALUE").toString() : "null");
+                    next.append(begin);
+                    next.append("added with value: ");
+                    Object value = map.get("NEW VALUE");
+                    if (value == null) {
+                        next.append("null");
+                    } else {
+                        if (value.getClass().isArray()) {
+                            next.append("[complex value]");
+                        } else {
+                            next.append(value);
+                        }
+                    }
+                    stringJoiner.add(next);
                     break;
                 case "UPDATE":
+                    next.append(begin);
                     next.append("updated. From ");
                     Object oldValue = map.get("OLD VALUE");
                     if (oldValue == null) {
                         next.append("null to ");
-                    } else if (!oldValue.getClass().isPrimitive()) {
+                    } else if (oldValue.getClass().isArray()) {
                         next.append("[complex value] to ");
                     } else {
-                        next.append(oldValue).append(" to");
+                        next.append(oldValue).append(" to ");
                     }
                     Object newValue = map.get("NEW VALUE");
                     if (newValue == null) {
                         next.append("null");
-                    } else if (!newValue.getClass().isPrimitive()) {
+                    } else if (newValue.getClass().isArray()) {
                         next.append("[complex value]");
                     } else {
                         next.append(newValue);
                     }
+                    stringJoiner.add(next);
                     break;
                 case "SAME":
                     break;
                 default:
                     throw new RuntimeException();
             }
-
-            stringJoiner.add(next);
         }
         return stringJoiner.toString();
     }
