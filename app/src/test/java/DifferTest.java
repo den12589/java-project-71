6fe47ac9ad380dd68c791be8fab4a7d4cf493e06
des12589 <d.makarov44@yandex.ru>
2024-09-18 18:25:31 +0300

    111

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -1,12 +1,11 @@
 import hexlet.code.Differ;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import picocli.CommandLine;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -15,45 +14,48 @@ class DifferTest {
     private static String expectedStylish;
     private static String expectedPlain;
     private static String expectedJson;
-    private static final String beginPathExpected = "src/test/resources/expected_results/";
-    private static final String beginPathFiles = "src/test/resources/input_files/";
+    private static final String BEGIN_PATH_EXPECTED = "src/test/resources/expected_results/";
+    private static final String BEGIN_PATH_FILES = "src/test/resources/input_files/";
 
     @BeforeAll
     static void setup() throws IOException {
-        expectedStylish = Files.readString(Paths.get(beginPathExpected + "resultForStylish.txt").toAbsolutePath().normalize()).trim();
-        expectedPlain = Files.readString(Paths.get(beginPathExpected + "resultForPlain.txt").toAbsolutePath().normalize()).trim();
-        expectedJson = Files.readString(Paths.get(beginPathExpected + "resultForJson.json").toAbsolutePath().normalize()).trim();
+        Path pathStylish = Paths.get(BEGIN_PATH_EXPECTED + "resultForStylish.txt").toAbsolutePath().normalize();
+        Path pathPlain = Paths.get(BEGIN_PATH_EXPECTED + "resultForPlain.txt").toAbsolutePath().normalize();
+        Path pathJson = Paths.get(BEGIN_PATH_EXPECTED + "resultForJson.json").toAbsolutePath().normalize();
+        expectedStylish = Files.readString(pathStylish).trim();
+        expectedPlain = Files.readString(pathPlain).trim();
+        expectedJson = Files.readString(pathJson).trim();
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"json", "yaml"})
     void testGenerateDefault(String fileFormat) throws IOException {
-        var file1 = beginPathFiles + "file1." + fileFormat;
-        var file2 = beginPathFiles + "file2." + fileFormat;
+        var file1 = BEGIN_PATH_FILES + "file1." + fileFormat;
+        var file2 = BEGIN_PATH_FILES + "file2." + fileFormat;
         assertEquals(expectedStylish, Differ.generate(file1, file2));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"json", "yaml"})
     void testGenerateStylish(String fileFormat) throws IOException {
-        var file1 = beginPathFiles + "file1." + fileFormat;
-        var file2 = beginPathFiles + "file2." + fileFormat;
+        var file1 = BEGIN_PATH_FILES + "file1." + fileFormat;
+        var file2 = BEGIN_PATH_FILES + "file2." + fileFormat;
         assertEquals(expectedStylish, Differ.generate(file1, file2, "stylish"));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"json", "yaml"})
     void testGeneratePlain(String fileFormat) throws IOException {
-        var file1 = beginPathFiles + "file1." + fileFormat;
-        var file2 = beginPathFiles + "file2." + fileFormat;
+        var file1 = BEGIN_PATH_FILES + "file1." + fileFormat;
+        var file2 = BEGIN_PATH_FILES + "file2." + fileFormat;
         assertEquals(expectedPlain, Differ.generate(file1, file2, "plain"));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"json", "yaml"})
     void testGenerateJson(String fileFormat) throws IOException {
-        var file1 = beginPathFiles + "file1." + fileFormat;
-        var file2 = beginPathFiles + "file2." + fileFormat;
+        var file1 = BEGIN_PATH_FILES + "file1." + fileFormat;
+        var file2 = BEGIN_PATH_FILES + "file2." + fileFormat;
         assertEquals(expectedJson, Differ.generate(file1, file2, "json"));
     }
 }
