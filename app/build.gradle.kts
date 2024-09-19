7c98a29eb92c294e9dc3e542e549b81542de5883
des12589 <d.makarov44@yandex.ru>
2024-09-19 23:04:25 +0300

    111

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,12 +14,16 @@ repositories {
 
 dependencies {
     implementation("info.picocli:picocli:4.7.6")
-    implementation ("com.fasterxml.jackson.core:jackson-databind:2.10.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.10.0")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.0-rc1")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
 }
+application {
+    mainClass = "hexlet.code.App"
+}
+
 
 tasks.test {
     useJUnitPlatform()
