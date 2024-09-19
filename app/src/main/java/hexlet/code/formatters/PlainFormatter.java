dfa8bff441e0e01595c6ca2bad912859f7aa03cb
des12589 <d.makarov44@yandex.ru>
2024-09-19 13:59:02 +0300

    111

--- app/src/main/java/hexlet/code/formatters/PlainFormatter.java
+++ app/src/main/java/hexlet/code/formatters/PlainFormatter.java
@@ -9,16 +9,16 @@ public class PlainFormatter {
     public static String format(List<Map<String, Object>> compareList) {
         StringJoiner result = new StringJoiner("\n");
         compareList.forEach(map -> {
+            String name = map.get("NAME").toString();
             String oldValue = createValueString(map.get("OLD VALUE"));
             String newValue = createValueString(map.get("NEW VALUE"));
-            String name = map.get("NAME").toString();
             switch (map.get("STATUS").toString()) {
+                case "ADD" -> result.add("Property '" + name + "' was added with value: " + newValue);
                 case "SAME" -> {
                 }
-                case "ADD" -> result.add("Property '" + name + "' was added with value: " + newValue);
+                case "REMOVED" -> result.add("Property '" + name + "' was removed");
                 case "UPDATE" ->
                         result.add("Property '" + name + "' was updated. From " + oldValue + " to " + newValue);
-                case "REMOVED" -> result.add("Property '" + name + "' was removed");
                 default -> throw new RuntimeException("Status isn't correct");
             }
         });
@@ -26,31 +26,15 @@ public class PlainFormatter {
     }
 
     private static String createValueString(Object o) {
-        StringBuilder result = new StringBuilder();
         if (Objects.isNull(o)) {
-            result.append("null");
+            return "null";
         }
-        if (isRecommendedType(o)) {
-            result.append("[complex value]");
+        if (o instanceof String) {
+            return "'" + o + "'";
         }
-        if (isString(o)) {
-            result.append("'").append(o).append("'");
+        if ((o instanceof Boolean) || (o instanceof Integer)) {
+            return o.toString();
         }
-        if (isBooleanOrInteger(o)) {
-            result.append(o);
-        }
-        return result.toString();
-    }
-
-    private static boolean isBooleanOrInteger(Object o) {
-        return (o instanceof Boolean) || (o instanceof Integer);
-    }
-
-    private static boolean isString(Object o) {
-        return o instanceof String;
-    }
-
-    private static boolean isRecommendedType(Object o) {
-        return !isString(o) && !isBooleanOrInteger(o) && !Objects.isNull(o);
+        return "[complex value]";
     }
 }
--- app/src/main/java/hexlet/code/formatters/StylishFormatter.java
+++ app/src/main/java/hexlet/code/formatters/StylishFormatter.java
@@ -10,27 +10,25 @@ public class StylishFormatter {
         StringJoiner result = new StringJoiner("\n");
         result.add("{");
         compareList.forEach(map -> {
-            String oldValue = Objects.isNull(map.get("OLD VALUE")) ? "null" : map.get("OLD VALUE").toString();
-            String newValue = Objects.isNull(map.get("NEW VALUE")) ? "null" : map.get("NEW VALUE").toString();
+            String name = map.get("NAME").toString();
+            String oldValue = createValueString(map.get("OLD VALUE"));
+            String newValue = createValueString(map.get("NEW VALUE"));
             switch (map.get("STATUS").toString()) {
-                case "ADD":
-                    result.add("  + " + map.get("NAME") + ": " + newValue);
-                    break;
-                case "REMOVED":
-                    result.add("  - " + map.get("NAME") + ": " + oldValue);
-                    break;
-                case "SAME":
-                    result.add("    " + map.get("NAME") + ": " + oldValue);
-                    break;
-                case "UPDATE":
-                    result.add("  - " + map.get("NAME") + ": " + oldValue);
-                    result.add("  + " + map.get("NAME") + ": " + newValue);
-                    break;
-                default:
-                    throw new RuntimeException("Status isn't correct");
+                case "ADD" -> result.add("  + " + name + ": " + newValue);
+                case "SAME" -> result.add("    " + name + ": " + oldValue);
+                case "REMOVED" -> result.add("  - " + name + ": " + oldValue);
+                case "UPDATE" -> {
+                    result.add("  - " + name + ": " + oldValue);
+                    result.add("  + " + name + ": " + newValue);
+                }
+                default -> throw new RuntimeException("Status isn't correct");
             }
         });
         result.add("}");
         return result.toString();
+    }
+
+    private static String createValueString(Object o) {
+        return Objects.isNull(o) ? "null" : o.toString();
     }
 }
